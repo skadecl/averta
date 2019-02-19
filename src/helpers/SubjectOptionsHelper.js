062a1f7d84c3989563fb9ce685712c137d79a4f3
Roberto Román <rf.romang@gmail.com>
2019-02-19 10:56:40 -0300

    Introduce SubjectOptionsHelper

---
+++ src/helpers/SubjectOptionsHelper.js
@@ -0,0 +1,26 @@
+const containsOption = (subjectLines, option) => (
+  subjectLines
+    .some(line => line.includes(option))
+);
+
+const build = subjectLines => {
+  const options = {
+    SKIP: '[svb skip]',
+    OVERRIDE_VERSION: '[svb over]',
+    FORCE_MAJOR: '[svb major]',
+    FORCE_MINOR: '[svb minor]',
+    FORCE_PATCH: '[svb patch]',
+    NO_TAG: '[svb notag]'
+  };
+
+  Object.keys(options)
+    .map((option) => {
+      options[option] = containsOption(subjectLines, options[option]);
+      return options[option];
+    });
+  return options;
+};
+
+export default {
+  build
+};
