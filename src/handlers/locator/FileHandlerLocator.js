9df5184f294c2e428b70f5c1cfe6f5473dc6f287
Roberto Román <rf.romang@gmail.com>
2019-02-21 11:58:50 -0300

    Fix FileHandlerLocator misbehavior

--- src/handlers/locator/FileHandlerLocator.js
+++ src/handlers/locator/FileHandlerLocator.js
@@ -3,12 +3,12 @@ import LogHelper from '../../helpers/LogHelper';
 
 const getHandler = (locatorString) => {
   const locators = locatorString.split('.');
-  let handlerInstance = null;
+  let handlerInstance = FileHandlers;
 
   locators.forEach((locator) => {
-    handlerInstance = FileHandlers[locator];
+    handlerInstance = handlerInstance[locator];
     if (!handlerInstance) {
-      LogHelper.throwException(`${locatorString} is not a valid FileHandler`);
+      LogHelper.throwException(`${locatorString} is not a valid FileHandler.`);
     }
   });
   return handlerInstance;
