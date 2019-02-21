0e2a45d46b0d23f1c1c1ac79da0342a7c225658f
Roberto Román <rf.romang@gmail.com>
2019-02-21 19:48:38 -0300

    Use ProcessHelper at app init

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 #! /usr/bin/env node
 
 import 'babel-polyfill';
-
+import ProcessHelper from './helpers/ProcessHelper';
 import Semverbot from './Semverbot';
+
+ProcessHelper.initProcessEventHandlers();
 
 Semverbot.init();
