3c09ec12925566dbff62137dac77592fa8b1cabc
Roberto Román <rf.romang@gmail.com>
2019-02-21 11:57:41 -0300

    Update config schema
    Now accepting multiple branches configuration

--- src/config/configSchema.js
+++ src/config/configSchema.js
@@ -1,66 +1,69 @@
 export default {
   id: '/Config',
-  type: 'object',
-  properties: {
-    branches: {
-      type: 'array',
-      required: true,
-      minItems: 1,
-      items: {
-        type: 'string'
-      }
-    },
-    versioning: {
-      type: 'object',
-      properties: {
-        major: {
-          required: true,
-          type: 'array',
-          items: {
-            type: 'string'
-          }
-        },
-        minor: {
-          required: true,
-          type: 'array',
-          items: {
-            type: 'string'
-          }
-        },
-        patch: {
-          required: true,
-          type: 'array',
-          items: {
-            type: 'string'
-          }
+  type: 'array',
+  items: {
+    type: 'object',
+    properties: {
+      branches: {
+        type: 'array',
+        required: true,
+        minItems: 1,
+        items: {
+          type: 'string'
         }
-      }
-    },
-    filesToUpdate: {
-      required: true,
-      type: 'object',
-      patternProperties: {
-        '^[^0-9][aA-zZ0-9]+([\.]{1}[^0-9][aA-zZ0-9]+)*\.[^0-9][aA-zZ0-9]+$': {
-          type: 'array',
-          items: {
-            type: 'object',
-            properties: {
-              url: {
-                type: 'string',
-                required: true
-              },
-              keys: {
-                type: 'array',
-                minItems: 1,
-                items: {
-                  type: 'string'
-                }
-              }
+      },
+      versioning: {
+        type: 'object',
+        properties: {
+          major: {
+            required: true,
+            type: 'array',
+            items: {
+              type: 'string'
+            }
+          },
+          minor: {
+            required: true,
+            type: 'array',
+            items: {
+              type: 'string'
+            }
+          },
+          patch: {
+            required: true,
+            type: 'array',
+            items: {
+              type: 'string'
             }
           }
         }
       },
-      additionalProperties: false
+      filesToUpdate: {
+        required: true,
+        type: 'object',
+        patternProperties: {
+          '^[^0-9][aA-zZ0-9]+([\.]{1}[^0-9][aA-zZ0-9]+)*\.[^0-9][aA-zZ0-9]+$': {
+            type: 'array',
+            items: {
+              type: 'object',
+              properties: {
+                url: {
+                  type: 'string',
+                  required: true
+                },
+                keys: {
+                  type: 'array',
+                  minItems: 1,
+                  items: {
+                    type: 'string'
+                  }
+                }
+              }
+            }
+          }
+        },
+        additionalProperties: false
+      }
     }
   }
 };
