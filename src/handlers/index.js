648117fdfaffdc7eeab7b391b1272b31d169036d
Roberto Román <rf.romang@gmail.com>
2019-02-21 19:48:16 -0300

    Move file handling iteration to FileHandlerHelper

--- src/handlers/index.js
+++ src/handlers/index.js
@@ -1,7 +1,19 @@
 import JSONFileChanger from './javascript/JSONFileChanger';
 import FileHandlerLocator from './locator/FileHandlerLocator';
+import LogHelper from '../helpers/LogHelper';
+
+const handleFileUpdate = (fileHandlers, newVersion) => {
+  const handlerLocators = Object.keys(fileHandlers);
+  handlerLocators.forEach((handlerLocator) => {
+    const handlerInstance = FileHandlerLocator.getHandler(handlerLocator);
+    const files = fileHandlers[handlerLocator];
+    handlerInstance.updateFiles(files, newVersion);
+  });
+  LogHelper.info('All files were updated successfully');
+};
 
 const FileHandlers = {
+  handleFileUpdate,
   Locator: FileHandlerLocator,
   Javascript: {
     JSON: JSONFileChanger
